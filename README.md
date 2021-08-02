d8fecc4875cac9d9eb0f1065b7b00c05da8e48ba
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2021-08-01 22:50:44 -0500

    Better README and module structure

--- README.md
+++ README.md
@@ -4,9 +4,7 @@
 
 Template for pygame games
 
-## Development
-
-### Setup
+## Setup
 
 You will need to create a virtual environment to develop or run this application:
 ```bash
@@ -19,6 +17,17 @@ Next, install the requirements:
 pip install -Ur requirements.txt
 ```
 
+## Running
+
+Before running the application, ensure you have sourced the virtual environment created above.
+
+From the root of the repo, run the following command to start the game:
+```bash
+python3 -m mygame
+```
+
+## Development
+
 ### Tests and Code Style
 
 This repo uses `tox` to run unit tests and perform code style cleanup and static analysis checks.
@@ -30,7 +39,7 @@ pip install tox
 
 Then, simply run `tox`.
 
-## Module Hierarchy
+### Module Hierarchy
 
 To avoid cyclical imports, there is a hierarchy of modules.
 Modules should only import from modules lower than them on
@@ -49,5 +58,5 @@ Thus, the `constants` module should not import any other modules.
 
 ## Game State
 
-Game state is handled by a central dataclass.
-Any state that should persist between play sessions should live in this class.
+Game state is handled by a central state class called `GameState`.
+This class is passed into the `Scene` objects which can reference and modify the game state as needed.
---
+++ mygame/src/state/asteroid_actor.py
@@ -0,0 +1,52 @@
+from random import randrange
+from typing import List
+
+import pygame
+import pygame.gfxdraw
+from pygame.surface import Surface
+
+from mygame.src.constants.game_constants import GAME_WIDTH_PX
+from mygame.src.interfaces.actor import Actor
+
+BASE_SPEED = 200
+MINIMUM_SIZE = 20
+MAXIMUM_SIZE = 150
+
+
+class Asteroid(Actor):
+    def __init__(self, pos_x: float = None, pos_y: float = None, size: float = None):
+        super().__init__()
+        if pos_x:
+            self.pos_x = pos_x
+        else:
+            self.pos_x = randrange(0, GAME_WIDTH_PX)
+
+        if size is None:
+            self.size = randrange(MINIMUM_SIZE, MAXIMUM_SIZE)
+        else:
+            self.size = size
+
+        if pos_y:
+            self.pos_y = pos_y
+        else:
+            self.pos_y = -self.size * 2
+
+        self.speed = float(MAXIMUM_SIZE - MINIMUM_SIZE) / self.size * BASE_SPEED
+
+    def get_collision_polygon(self) -> List[List[float]]:
+        point1 = [self.pos_x - self.size, self.pos_y - self.size]
+        point2 = [self.pos_x + self.size - 3, self.pos_y - 2]
+        point3 = [self.pos_x + 2, self.pos_y + self.size]
+        point4 = [self.pos_x - 5, self.pos_y]
+        return [point1, point2, point3, point4]
+
+    def process_input(self, events):
+        pass
+
+    def update(self, time_delta: float):
+        self.pos_y += self.speed * time_delta
+
+    def render(self, screen: Surface):
+        pygame.gfxdraw.aapolygon(screen, self.get_collision_polygon(), (255, 255, 255))
+        pygame.gfxdraw.filled_polygon(screen, self.get_collision_polygon(), (255, 255, 255))
+        pygame.gfxdraw.aapolygon(screen, self.get_collision_polygon(), (200, 200, 200))
